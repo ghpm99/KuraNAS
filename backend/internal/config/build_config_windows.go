3ff5afa449f921eb9fb3fdc6caef04f625d30da3
Guilherme H. <ghpm99@gmail.com>
2025-05-23 00:42:01 -0300

    hotfix: windows path

--- backend/internal/config/build_config_windows.go
+++ backend/internal/config/build_config_windows.go
@@ -13,11 +13,11 @@ func GetBuildConfig(key string) string {
 	case "BuildVersion":
 		return "windows"
 	case "DbPath":
-		return fmt.Sprintf("%s/kuranas/db.sqlite3", os.TempDir())
+		return fmt.Sprintf("%s/Kuranas/db.sqlite3", os.TempDir())
 	case "IconPath":
-		return fmt.Sprintf("%s/kuranas/icons/", os.Getenv("ProgramFiles"))
+		return fmt.Sprintf("%s/Kuranas/icons/", os.Getenv("ProgramFiles"))
 	case "TranslationsPath":
-		return fmt.Sprintf("%s/kuranas/translations/", os.Getenv("ProgramFiles"))
+		return fmt.Sprintf("%s/Kuranas/translations/", os.Getenv("ProgramFiles"))
 	default:
 		return ""
 	}
